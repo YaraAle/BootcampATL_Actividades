035eeede11184fbf91223c32015a9c51a2430341
Yara Romero <yaraleromeral@gmail.com>
2023-06-06 17:21:25 -0400

    Resolviendo ejercicio 2

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*
+2) Conversor de millas a kilómetros:
+Pídele al usuario que ingrese una distancia en millas.
+Convierte la distancia a kilómetros utilizando la fórmula: kilómetros = millas * 1.60934.
+Muestra la distancia en kilómetros al usuario.
+*/
+public class Ejercicio2 {
+    public static void main(String[] args){
+        Scanner pedirDatos = new Scanner(System.in);
+        System.out.println("Ingresar distancia en millas");
+        Double distanciaMillas=pedirDatos.nextDouble();
+        Double distanciaKilometros=distanciaMillas*1.60934;
+        System.out.println("La distancia en kilometros es: "+distanciaKilometros);
+    }
+}
