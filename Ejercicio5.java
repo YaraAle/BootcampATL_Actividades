097e1289562c8ea9222a049deb85e47c1a9f12d9
Yara Romero <yaraleromeral@gmail.com>
2023-06-06 17:47:18 -0400

    Resolviendo ejercicio 5

---
+++ Ejercicio5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+5) Juego de adivinar el número:
+Genera un número aleatorio entre 1 y 100. Pídele al usuario que adivine el número.
+*/
+public class Ejercicio5 {
+    public static void main(String[] args){
+        Integer numeroAleatorio= (int) (Math.random()*100+1);
+        Scanner pedirDatos = new Scanner(System.in);
+        System.out.println("Juguemos a la adivinanza. Ingresa un numero entre el 1 al 100");
+        Integer numero=pedirDatos.nextInt();
+        if(numero==numeroAleatorio){
+            System.out.println("Acertaste, el numero a adivinar es: "+numeroAleatorio);
+        }else{
+            System.out.println("Lo siento, el numero era: "+numeroAleatorio);
+        }
+
+    }
+}
