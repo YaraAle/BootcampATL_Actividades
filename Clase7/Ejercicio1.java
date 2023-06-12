1d273e19b1a1bdf8ce1f8faa3f99c1f5eca7a595
Yara Romero <yaraleromeral@gmail.com>
2023-06-12 18:23:43 -0400

    Resolviendo ejercicios 1 2 y 3

--- Clase7/Ejercicio1.java
+++ Clase7/Ejercicio1.java
@@ -3,7 +3,11 @@ package Clase7;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/*Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.*/
+/*1) Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.*/
+/*2) Después de realizar el ejercicio anterior, agregar que muestre el número mayor y el número menor.*/
+/*3) Ahora, hay que mostrar el promedio.
+Para hacer esto se suman todos los números y se divide por 5,
+ya que el usuario agregó 5 números.*/
 public class Ejercicio1 {
     public static void main(String[] args){
         Scanner pedirDatos = new Scanner(System.in);
@@ -13,5 +17,24 @@ public class Ejercicio1 {
             numeros[i] = pedirDatos.nextInt();
         }
         System.out.println("Los numeros ingresados son:"+ Arrays.toString(numeros));
+
+        int menor = numeros[0];
+        int mayor = numeros[0];
+        int suma = 0;
+        for (int i=0; i<numeros.length; i++){
+            if(numeros[i]<menor){
+                menor = numeros[i];
+            }
+            if(numeros[i]>mayor){
+                mayor = numeros[i];
+            }
+            suma = suma+numeros[i];
+        }
+        System.out.println("El número mayor es: "+mayor +", "+"El número menor es: "+menor);
+
+        int promedio=suma/numeros.length;
+        System.out.println("La sumatoria de los numeros ingresados es: " + suma);
+        System.out.println("El promedio es: " + promedio);
+
     }
 }
